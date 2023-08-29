061d7c4254628eed45d26a6113a25195ac331617
German Bertea <gerbertea@gmail.com>
2023-08-29 11:26:48 +0200

    Add posts with emogis, db changes, add post server api and routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import Head from "next/head";
 import Link from "next/link";
-import { SignIn, SignOutButton,SignInButton, useUser } from "@clerk/nextjs";
+import { SignIn, SignOutButton, SignInButton, useUser } from "@clerk/nextjs";
 
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
+  console.log(data);
 
 
   return (
@@ -17,12 +21,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      
-      <div>
-        {!user.isSignedIn && <SignInButton />}
-        {!!user.isSignedIn && <SignOutButton />}
-      </div>
-      
+
+        <div>
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map(post => (
+            <div key={post.id}> {post.content} </div>
+          ))}
+        </div>
+
       </main>
     </>
   );
