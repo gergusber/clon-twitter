713bcb770790dcac3102acd07ed2893aa6d8715f
German Bertea <gerbertea@gmail.com>
2023-08-29 20:54:23 +0200

    Addd tweets, styles, add postVIew,remove unused configs

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,9 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    domains: ['images.clerk.dev'],
+  },
 };
 
 export default config;
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,9 +1,42 @@
+import { clerkClient } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl
+  }
+}
+
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100
+    });
+
+    const users = (await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100
+    })).map(filterUserForClient)
+
+    return posts.map(post => {
+      const author = users.find(usr => usr.id === post.authorId)
+
+      if (!author?.username) throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message: 'Author for post not Found.' })
+
+
+      return {
+        post,
+        author:{
+          ...author,
+          username: author.username
+        }
+      }
+    })
   }),
 });
