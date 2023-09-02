7cc0def3ddd671b06734f86f4d9ad92414fa5a71
German Bertea <gerbertea@gmail.com>
2023-09-02 17:25:16 +0200

    fix layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (<main className="flex h-screen justify-center ">
-    <div className="h-full w-full overflow-y-scroll no-scrollbar border-x border-slate-400 md:max-w-2xl ">
+    <div className="h-full w-full overflow-y no border-x border-slate-400 md:max-w-2xl ">
       <div> {props.children} </div>
     </div>
   </main>
