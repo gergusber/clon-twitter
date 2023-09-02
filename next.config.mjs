ad700853e20b3c437fe31ebc35665d0296732007
German Bertea <gerbertea@gmail.com>
2023-09-02 18:12:16 +0200

    build faster commits on vercell without checking typescript errors

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,14 @@ const config = {
   images: {
     domains: ['images.clerk.dev'],
   },
+
+  typescript:{
+    ignoreBuildErrors: true
+  },
+  eslint:{
+    ignoreDuringBuilds:true
+  },
+  swcMinify: true
 };
 
 export default config;
